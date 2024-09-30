4a6783fc4b1af6b74c507ecd707557efb98cc234
itarcticportal <it@arcticportal.org>
2024-09-30 16:16:51 +0000

    All containers able to boot

---
+++ wagtail/ropon/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-$t2b@&=)*2ts41g-ybjf_kb*dydu@yzgdu-t#d2fo!$c+=a5kh"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
