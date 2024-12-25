488f201d5ca3cfb10969940b7f58657dda6c284c
Arundeep Singh <arundeep.singh@outlook.com>
2024-12-25 19:25:19 +0000

    updated hide admin pages

--- wagtail/ropon/wagtail_hooks.py
+++ wagtail/ropon/wagtail_hooks.py
@@ -5,9 +5,7 @@ from wagtail import hooks
 # Hide the pages menu item for non-superusers
 @hooks.register('construct_main_menu')
 def hide_pages_menu(request, menu_items):
-    print(f"{request.user}")
-    print(f"{request.user.is_superuser}")
-    print(f"{[item.name for item in menu_items]}")
+   
     if not request.user.is_superuser:
         menu_items[:] = [item for item in menu_items if item.name != 'explorer']
 
