8fad8be6a9cf422edf1aa4f1ab2a36adfa3f5670
Arundeep Singh <arundeep.singh@outlook.com>
2025-01-23 23:23:52 +0000

    Update load_ropon_cvs management to check if existing data

--- backend/ropon_data/management/commands/load_ropon_cvs.py
+++ backend/ropon_data/management/commands/load_ropon_cvs.py
@@ -1,12 +1,10 @@
-# wagtail/ropon_data/management/commands/load_ropon_cvs.py
-
 import os
 import glob
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from pathlib import Path
 import logging
-
+from django.apps import apps
 
 FIXTURES_PATTERN = '*_fixtures.json'
 
@@ -30,15 +28,35 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING('No fixture files found.'))
             return
 
+        n_loaded_fixtures = 0
+        n_data_exists = 0
         for fixture_file in fixture_files:
             fixture_name = os.path.basename(fixture_file)
             # Remove the file extension to get the fixture name
             fixture_name = os.path.splitext(fixture_name)[0]
+            
+            # Check if there is existing data in the relevant table
+            model_name = fixture_name.split('_')[0].capitalize()
+            model = apps.get_model('ropon_data', model_name)
+            if model.objects.exists():
+                self.stdout.write(self.style.NOTICE(f'Data already exists in the {model_name} table. Skipping fixture: {fixture_name}'))
+                n_data_exists += 1
+                continue
+
             self.stdout.write(f'Loading fixture: {fixture_name}')
             try:
                 call_command('loaddata', fixture_name)
+                n_loaded_fixtures += 1
             except Exception as e:
                 self.stdout.write(self.style.ERROR(f'Error loading fixture {fixture_name}: {e}'))
                 continue
+        
+        
+        if n_loaded_fixtures == len(fixture_files):
+            self.stdout.write(self.style.SUCCESS('All fixtures loaded successfully.'))
+        
+        if n_data_exists > 0:
+            self.stdout.write(self.style.NOTICE(f'{n_data_exists}/{len(fixture_files)} fixtures skipped because data already exists.'))
+            self.stdout.write(self.style.NOTICE(f'{n_loaded_fixtures}/{len(fixture_files)} fixtures loaded successfully.'))
 
-        self.stdout.write(self.style.SUCCESS('All fixtures loaded successfully.'))
+        
