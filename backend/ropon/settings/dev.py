bf6f4137a9a27cd668d4ae3ba7be18ecf3976441
Arundeep Singh <arundeep.singh@outlook.com>
2025-03-02 00:31:32 +0000

    New feature: Configure wagtail to send emails Fixes #18

--- backend/ropon/settings/dev.py
+++ backend/ropon/settings/dev.py
@@ -6,7 +6,23 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-$t2b@&=)*2ts41g-ybjf_kb*dydu@yzgdu-t#d2fo!$c+=a5kh"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+# Django Email server settings SMTP backend
+# https://docs.djangoproject.com/en/5.1/topics/email/
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env.str('EMAIL_HOST', default='smtp.hostinger.com')
+EMAIL_PORT = env.int('EMAIL_PORT', default=465)
+EMAIL_USE_TLS = env.bool('EMAIL_USE_TLS', default=False)
+EMAIL_HOST_USER = env.str('EMAIL_HOST_USER','admin@arcdata.is')
+EMAIL_HOST_PASSWORD = env.str('EMAIL_HOST_PASSWORD','1qazXSW"1qazXSW"')
+EMAIL_USE_SSL = env.bool('EMAIL_USE_SSL', default=True)
+EMAIL_TIMEOUT = env.int('EMAIL_TIMEOUT', default=5)
+DEFAULT_FROM_EMAIL = env.str('DEFAULT_FROM_EMAIL', 'admin@arcdata.is')
+
+
 
 LOGGING = {
     'version': 1,
@@ -23,8 +39,3 @@ LOGGING = {
         },
     },
 }
-
-try:
-    from .local import *
-except ImportError:
-    pass
