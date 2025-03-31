78001ba982e4e8100363693467214fbcd436c818
Arundeep Singh <arundeep.singh@outlook.com>
2025-03-31 20:30:50 +0000

    backend: Add wagtail-guide package Fixes #77

--- backend/ropon/templatetags/ropon_tags.py
+++ backend/ropon/templatetags/ropon_tags.py
@@ -6,16 +6,15 @@ This provide template variables specific for ROPON application.
 from django import template
 from django.conf import settings
 
-from wagtail_guide.settings import wagtail_guide_settings
 register = template.Library()
 
 
 @register.simple_tag
 def frontend_url():
     """
-    Returns the frontend URL from settings.
+    This tag retrieves the FRONTEND_URL setting from Django settings.
+   
     """
-    # This tag retrieves the FRONTEND_URL setting from Django settings.
     
     return getattr(settings, "FRONTEND_URL", None)
 
@@ -24,9 +23,9 @@ def frontend_url():
 @register.simple_tag
 def wagtail_guide_title():
     """
-    Returns the frontend URL from settings.
+    Returns the Wagtail Guide menu label from wagtail_guide_settings.
     """
-    # This tag retrieves the FRONTEND_URL setting from Django settings.
     
+    wagtail_guide_settings = getattr(settings, "WAGTAIL_GUIDE_SETTINGS", {})
     return getattr(wagtail_guide_settings, "WAGTAIL_GUIDE_MENU_LABEL", "Editor's Guide")
 
