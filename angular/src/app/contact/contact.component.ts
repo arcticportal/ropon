8d78737e54347a2e0748d5da2bc71d884645015d
itarcticportal <it@arcticportal.org>
2025-06-11 10:08:44 +0000

    Fixed logo location and implemented Contact form

---
+++ angular/src/app/contact/contact.component.ts
@@ -0,0 +1,43 @@
+import { Component, HostBinding, inject } from '@angular/core';
+import {NgIf} from '@angular/common'
+import {FormBuilder, FormGroup, Validators,
+	ReactiveFormsModule} from '@angular/forms'
+import {HttpClient} from '@angular/common/http'
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [NgIf, ReactiveFormsModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.css'
+})
+export class ContactComponent {
+  @HostBinding('class.container') container = true
+  submissionState: 'idle' | 'submitting' | 'success' | 'error' = 'idle'
+  errorMessage: string = ''
+  private fb = inject(FormBuilder)
+  private http = inject(HttpClient)
+  contactForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(2)]],
+    from_email_id: ['', [Validators.required, Validators.email]],
+    message: ['', [Validators.required, Validators.minLength(10)]]})
+
+  onSubmit() {
+    if (this.contactForm.invalid) return
+    this.submissionState = 'submitting'
+    const formData = this.contactForm.value
+    this.http.post(
+      'https://wagtail.ropon.dev.cntb.arcticportal.org/api/v2/email/contact-us/',
+      formData).subscribe({
+	next: () => {
+	  this.submissionState = 'success'
+	  this.contactForm.reset() },
+	error: err => {
+	  this.submissionState = 'error'
+	  this.errorMessage = err.error?.message ||
+	    'An unknown error occurred' }}) }
+
+  get name() { return this.contactForm.get('name') }
+  get email() { return this.contactForm.get('from_email_id') }
+  get message() { return this.contactForm.get('message') }
+}
---
+++ wagtail/.bash_history
@@ -0,0 +1,131 @@
+l
+cd ..
+l
+wagtail start ropon wagtail
+cd wagtail
+l
+mv requirements.txt requirements.txt~
+cd ..
+wagtail start ropon wagtail
+cd ~
+l
+rm manage.py Dockerfile
+cd ..
+wagtail start ropon wagtail
+l
+cd wagtail
+l
+l -l
+exit
+l
+./manage.py makemigrations
+./manage.py migrate
+exit
+./manage.py createsuperuser
+exit
+./manage.py shell
+exit
+./manage.py shell
+exit
+/.manage.py shell
+l
+./manage.py shell
+l
+cd ropon
+l
+cd ..
+cat requirements.txt
+l
+pwd
+sudo iptables -L
+l
+exit
+l
+cat requirements.txt
+cat run
+pip install --save wagtail-tag-manager
+which python
+python --version
+exit
+l
+pip freeze >requirements.txt~
+cat requirements.txt
+cat requirements.txt~
+exit
+l
+pip install -r requirements.txt
+l
+mv freeze.txt freeze_old.txt
+pip freeze >freeze.txt
+cd home
+l
+cd migrations
+l
+cat 0002*
+cd ..
+l
+cd ..
+l
+cd ropon
+l
+cd ..
+l
+./manage.py migrate cookie_consent
+l
+cd ropon
+l
+pip show django-cookie-consent
+l
+cd ..
+l
+cd .local
+l
+cd ..
+l
+rm -r .local~
+cd .local
+l
+cd lib
+l
+cd ../bin
+l
+cd ../lib
+l
+cd python*
+l
+cd site*
+l
+cd cookie_consent
+l
+cd models
+cat models.py
+l
+exit
+ipython
+./manage.py shell
+l
+./manage.py collectstatic
+exit
+l
+./manage.py collectstatic
+exit
+l
+./manage.py makemigrations
+./manage.py migrate
+exit
+l
+./manage.py startapp network
+l
+exit
+l
+./manage.py makemigrations
+./manage.py migrate
+./manage.py makemigrations
+./manage.py migrate
+exit
+ipython3
+l
+les requirements.txt
+exit
+cat /etc/resolv.conf
+exit
