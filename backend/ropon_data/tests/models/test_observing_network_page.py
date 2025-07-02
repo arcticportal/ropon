b0ed77ce50b715f5ea54597a03f3a85c7576c8f4
Arundeep Singh <arundeep.singh@outlook.com>
2025-07-02 20:53:29 +0000

    Update logo URL in tests for ObservingNetworkPage and AgingNetworksReport

--- backend/ropon_data/tests/models/test_observing_network_page.py
+++ backend/ropon_data/tests/models/test_observing_network_page.py
@@ -111,7 +111,7 @@ class ObservingNetworkPageTests(WagtailPageTestCase):
             'abbreviation': 'TN',
             'description': 'A test network',
             'website_url': 'http://example.com',
-            'logo_url': 'https://polarobservingregistry.org/assets/ropon-text.png',
+            'logo_url': 'https://polarobservingregistry.org/logo_CU_Boulder.png',
             # 'ropon_id': '12345',
             'contact': 'contact@example.com',
             'has_catalog': 'yes'
@@ -147,7 +147,7 @@ class ObservingNetworkPageTests(WagtailPageTestCase):
             'abbreviation': 'TN',
             'description': 'A test network',
             'website_url': 'http://example.com',
-            'logo_url': 'https://polarobservingregistry.org/assets/ropon-text.png',
+            'logo_url': 'https://polarobservingregistry.org/logo_CU_Boulder.png',
             'ropon_id': '12345',
             'contact': 'contact@example.com',
             'has_catalog': 'yes',
